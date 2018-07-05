251bd3529ab65e485f635cbf2267ebd3c5c71f40
Max Dobeck <msdobeck@gmail.com>
2018-07-05 12:38:42 -0700

    Updating strings for domain.

--- authentication/handles.go
+++ authentication/handles.go
@@ -47,9 +47,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	if os.Getenv("GO_ENV") == "dev" {
 		domain = "127.0.0.1:3000"
 	} else if os.Getenv("GO_ENV") == "test" {
-		domain = "http://s3-sih-test.s3-website-us-west-1.amazonaws.com"
+		domain = "s3-sih-test.s3-website-us-west-1.amazonaws.com"
 	} else if os.Getenv("GO_ENV") == "prod" {
-		domain = "https://schedulingishard.com"
+		domain = "schedulingishard.com"
 	}
 	// Limit the sessions to 1 24-hour day
 	session.Options.MaxAge = 86400 * 1
