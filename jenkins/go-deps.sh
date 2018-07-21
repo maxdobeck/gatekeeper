3cd90ca05cc841db8bf12eae20b1a3f08a131133
Max Dobeck <msdobeck@gmail.com>
2018-07-20 21:38:46 -0700

    Added jenkins folder to hold scripts.

---
+++ jenkins/go-deps.sh
@@ -0,0 +1,7 @@
+sh 'go get github.com/antonlindstrom/pgstore'
+sh 'go get github.com/gorilla/context'
+sh 'go get github.com/gorilla/csrf'
+sh 'go get github.com/gorilla/mux'
+sh 'go get github.com/lib/pq'
+sh 'go get github.com/rs/cors'
+sh 'go get github.com/urfave/negroni'
