afdcae3280262bbb9d2f1b097c3fd33789f4b6b9
Max Dobeck <msdobeck@gmail.com>
2018-07-22 16:27:15 -0700

    Trying to install instead.

--- jenkins/scripts/go-build.sh
+++ jenkins/scripts/go-build.sh
@@ -2,4 +2,4 @@
 
 sh '/go/src/github.com/maxdobeck/gatekeeper/git checkout create-jenkinsfile && /go/src/github.com/maxdobeck/gatekeeper/git pull'
 sh '/go/src/github.com/maxdobeck/gatekeeper/go get ./...'
-sh '/go/src/github.com/maxdobeck/gatekeeper/go build'
+sh '/go/src/github.com/maxdobeck/gatekeeper/go install'
