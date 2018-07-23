bad3d09f741085cc954955eb0291773976fa1d78
Max Dobeck <msdobeck@gmail.com>
2018-07-22 17:15:19 -0700

    Removed superfluous script file.

--- jenkins/scripts/go-build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-sh 'cd /go/src/github.com/maxdobeck/gatekeeper && git checkout create-jenkinsfile && git pull'
-sh 'cd /go/src/github.com/maxdobeck/gatekeeper && go get ./...'
-sh 'cd /go/src/github.com/maxdobeck/gatekeeper && go install'
