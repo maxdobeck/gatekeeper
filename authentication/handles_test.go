7f8d19c3b33247895d7e179b685d7b4c13c3355f
Max Dobeck <msdobeck@gmail.com>
2018-07-28 20:23:49 -0700

    Fixed the test names.

--- authentication/handles_test.go
+++ authentication/handles_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 // An HTTP test to ensure a login request is rejected if the credentials are wrong
-func TestLoginGoodCredentials(t *testing.T) {
+func TestLoginBadCredentials(t *testing.T) {
 	bodyReader := strings.NewReader(`{"email": "WrongEmail@email.com", "password": "wrongPassword"}`)
 
 	req, err := http.NewRequest("POST", "/login", bodyReader)
@@ -36,7 +36,7 @@ func TestLoginGoodCredentials(t *testing.T) {
 }
 
 // Test the Login command with a valid set of credentials
-func TestLoginBadCredentials(t *testing.T) {
+func TestLoginGoodCredentials(t *testing.T) {
 	models.ConnToDB(os.Getenv("PGURL"))
 	// Signup a user
 	signupBody := strings.NewReader(`{"email": "testValidCreds@gmail.com", "email2":"testValidCreds@gmail.com", "password": "supersecret", "password2":"supersecret", "name":"Valid User Signup"}`)
@@ -56,6 +56,7 @@ func TestLoginBadCredentials(t *testing.T) {
 	Login(w, req)
 
 	resp := w.Result()
+	fmt.Println(resp.StatusCode)
 
 	if resp.StatusCode != 200 {
 		t.Fail()
