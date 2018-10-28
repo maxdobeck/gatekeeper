680e8d6ff42918f996219d73507365362d4e4598
Max Dobeck <msdobeck@gmail.com>
2018-10-27 19:16:25 -0700

    Fixed issue with the teardown script not having a cascade ability.

--- db/util/teardown.sql
+++ db/util/teardown.sql
@@ -1,6 +1,8 @@
 DELETE FROM enrollments;
 DELETE FROM members;
 DELETE FROM schedules;
+DELETE FROM shifts;
 DROP TABLE enrollments;
+DROP TABLE members CASCADE;
 DROP TABLE schedules;
-DROP TABLE members;
+DROP TABLE shifts;
