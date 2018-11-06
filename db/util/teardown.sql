b206a4f374bf11724a9d1629b5262cf630b92133
Max Dobeck <msdobeck@gmail.com>
2018-11-05 20:22:24 -0800

    Made teardown process better by removingg things with dependencies first.

--- db/util/teardown.sql
+++ db/util/teardown.sql
@@ -3,6 +3,6 @@ DELETE FROM members;
 DELETE FROM schedules;
 DELETE FROM shifts;
 DROP TABLE enrollments;
-DROP TABLE members CASCADE;
-DROP TABLE schedules CASCADE;
 DROP TABLE shifts CASCADE;
+DROP TABLE schedules CASCADE;
+DROP TABLE members CASCADE;
