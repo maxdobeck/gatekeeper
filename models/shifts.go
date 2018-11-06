ff1b1563907950924f658a07fa22246957875f2b
Max Dobeck <msdobeck@gmail.com>
2018-11-05 21:10:14 -0800

    Added clearer comments about structs and funcs.

--- models/shifts.go
+++ models/shifts.go
@@ -6,20 +6,20 @@ import (
 	"strings"
 )
 
-// Shift contains the core values
+// Shift contains the start, end, title, days worked, stop date, minimum enrollees, and owning schedule
 type Shift struct {
 	ID, Title, Start, End, Stop, MinEnrollees, Schedule string
 	Days                                                [7]string
 }
 
-// CreateShift builds a new schedule with the creator as the Owner
+// CreateShift builds a new shift and attaches it to a schedule
 func CreateShift(s *Shift) error {
 	d := week(s.Days)
 	min, _ := strconv.Atoi(s.MinEnrollees)
 	_, err := Db.Query("INSERT INTO shifts(title, start_time, end_time, stop_date, min_enrollees, schedule_id, sun, mon, tue, wed, thu, fri, sat) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)",
 		s.Title, s.Start, s.End, s.Stop, min, s.Schedule, d[0], d[1], d[2], d[3], d[4], d[5], d[6])
 	if err != nil {
-		log.Println("Error creating shift: ", err)
+		log.Println("Error creating shift with schedule: ", err, s)
 		return err
 	}
 	log.Println("Shift Created: ", s)
