34b418f1fa2b8f341e4c0d9e1ebea1d61ca17322
Max Dobeck <msdobeck@gmail.com>
2018-11-05 22:05:50 -0800

    Added test to make sure if we created 3 shifts we would get 3 shifts using getShifts().

--- models/shifts_test.go
+++ models/shifts_test.go
@@ -72,6 +72,105 @@ func TestCreateShift(t *testing.T) {
 	cleanupShifts()
 }
 
+// TestGetShifts creates a new shift
+func TestGetShifts(t *testing.T) {
+	ConnToDB(os.Getenv("PGURL"))
+
+	_, delErr := Db.Query("DELETE FROM schedules WHERE title like 'Test Shift'")
+	fmt.Println(delErr)
+
+	spoofShifts()
+
+	rows, errors := Db.Query("SELECT id FROM members LIMIT 1;")
+	if errors != nil {
+		fmt.Println(errors)
+	}
+	defer rows.Close()
+	var memberID string
+	for rows.Next() {
+		err := rows.Scan(&memberID)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Member ID used for testing: ", memberID)
+	}
+
+	srows, serrors := Db.Query("SELECT id FROM schedules LIMIT 1;")
+	if serrors != nil {
+		fmt.Println(serrors)
+	}
+	defer srows.Close()
+	var scheduleID string
+	for srows.Next() {
+		rerr := srows.Scan(&scheduleID)
+		if rerr != nil {
+			fmt.Println(rerr)
+		}
+		fmt.Println("Schedule ID used for test Shift: ", scheduleID)
+	}
+
+	s := Shift{
+		Title:        "Test Shift",
+		Schedule:     scheduleID,
+		Start:        "715",
+		End:          "1200",
+		MinEnrollees: "1",
+		Days:         [7]string{"", "Monday", "Tuesday", "", "", "Friday", ""},
+	}
+
+	s2 := Shift{
+		Title:        "Test Shift 2",
+		Schedule:     scheduleID,
+		Start:        "1015",
+		End:          "1700",
+		MinEnrollees: "1",
+		Days:         [7]string{"", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", ""},
+	}
+
+	s3 := Shift{
+		Title:        "Test Shift 3",
+		Schedule:     scheduleID,
+		Start:        "1200",
+		End:          "1200",
+		MinEnrollees: "1",
+		Days:         [7]string{"Sunday", "", "", "", "", "Friday", "Saturday"},
+	}
+
+	var newShiftError error
+	newShiftError = CreateShift(&s)
+	if newShiftError != nil {
+		fmt.Println("Test failed while creating new shift: ", newShiftError)
+		t.Fail()
+	}
+
+	newShiftError = CreateShift(&s2)
+	if newShiftError != nil {
+		fmt.Println("Test failed while creating new shift: ", newShiftError)
+		t.Fail()
+	}
+
+	newShiftError = CreateShift(&s3)
+	if newShiftError != nil {
+		fmt.Println("Test failed while creating new shift: ", newShiftError)
+		t.Fail()
+	}
+
+	shifts, getErr := GetShifts(scheduleID)
+	if getErr != nil {
+		fmt.Println("Failed due to: ", getErr)
+		t.Fail()
+	}
+
+	if len(shifts) < 3 {
+		t.Error("Expected 3 shifts and got fewer than 3.")
+	}
+	if len(shifts) != 3 {
+		t.Error("Expected 3 shifts and did not get 3")
+	}
+
+	cleanupShifts()
+}
+
 // Helpers for shift tests
 func spoofShifts() {
 	m := NewMember{
